167850e36da8fceab624671b9d0d3f60fd6cacfc
Tor <55250905+trer@users.noreply.github.com>
2021-04-03 02:16:11 +0200

    Create Dag6.js

---
+++ Dag6.js
@@ -0,0 +1,8 @@
+function ingenTall() {
+  var ingen = "".length
+  var en = "e".length
+  var ti = "eeeeeeeeee".length
+  var ni = 'eeeeeeeee'.length
+  var syv = 'eeeeeee'.length
+  return ti*ti*ti + ni*ti*ti + syv*ti+ syv
+}
